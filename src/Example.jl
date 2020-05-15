7c4ddbce83a565cf55e4ed8c10dac75f9efc7f65
Noah <noahx23@gmail.com>
2020-05-15 13:26:13 -0500

    ADD the mf_f function
    demonstrate how to use testing environments in JUNO

--- src/Example.jl
+++ src/Example.jl
@@ -1,5 +1,7 @@
 module Example
 
 greet() = print("Hello World!")
+include("extra.jl")
 
+export my_f
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,10 @@
 using Example
 using Test
 
+my_f(2,1)
+
 @testset "Example.jl" begin
     # Write your own tests here.
+
+    @test my_f(2,1) == 5
 end
